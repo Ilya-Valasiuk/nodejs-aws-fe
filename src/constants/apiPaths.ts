87816be4b30d2b56b94f50874cc8d06d4b45fbe9
Ilya Valasiuk <ilya.valasiuk@coxautoinc.com>
2020-11-24 15:58:00 +0300

    fix import service path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://48elrk5443.execute-api.us-east-1.amazonaws.com/dev',
+  import: 'https://ij8xh24vb3.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://rm4cc318y5.execute-api.us-east-1.amazonaws.com/dev'
 };
 
