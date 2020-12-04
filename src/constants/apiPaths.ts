466174c0196201653cb10e9028a938075d7cc091
Ilya Valasiuk <ilya.valasiuk@coxautoinc.com>
2020-12-04 21:06:30 +0300

    add cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ij8xh24vb3.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://rm4cc318y5.execute-api.us-east-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://ilya-valasiuk-cart-api-dev.us-east-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
